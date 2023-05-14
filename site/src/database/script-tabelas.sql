cca7b40e9ac44368ae48328f0cef26b4ed13556a
matheusslima19 <matheus.slima@sptech.school>
2023-05-13 22:33:49 -0300

    Validações e integrações 98%

---
+++ site/src/database/script-tabelas.sql
@@ -0,0 +1,36 @@
+-- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
+-- Você precisa executar os comandos no banco de dados para criar as tabelas,
+-- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+
+/*
+comandos para mysql - banco local - ambiente de desenvolvimento
+*/
+
+create database ValorantStats;
+use ValorantStats;
+
+
+CREATE TABLE usuario (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	nome VARCHAR(50),
+	email VARCHAR(50),
+	senha VARCHAR(50),
+	cpf CHAR(11)
+);
+
+
+
+/*
+comandos para criar usuário em banco de dados azure, sqlserver,
+com permissão de insert + update + delete + select
+*/
+
+-- CREATE USER [usuarioParaAPIWebDataViz_datawriter_datareader]
+-- WITH PASSWORD = '#Gf_senhaParaAPIWebDataViz',
+-- DEFAULT_SCHEMA = dbo;
+
+-- EXEC sys.sp_addrolemember @rolename = N'db_datawriter',
+-- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
+
+-- EXEC sys.sp_addrolemember @rolename = N'db_datareader',
+-- @membername = N'usuarioParaAPIWebDataViz_datawriter_datareader';
